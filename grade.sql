0e20c052c7d65afcae79f10ab5ca04e4890fd65e
Reyson <reysonteixeira@gmail.com>
2025-01-15 22:42:46 -0300

    include log

---
+++ grade.sql
@@ -0,0 +1,24 @@
+CREATE TABLE  tblEscola (
+  `idEscola` INT NOT NULL auto_increment,
+  `nomeEscola` VARCHAR(200) not NULL,
+  `registroInep` varchar(20) not null,
+    `logradouro`varchar(200) null,
+	`numero` int,
+ `complemento` varchar(200) null,
+`bairro` varchar(200) null,
+`cidade` varchar(200) null,
+`estado` varchar(2) null,
+ `cep` varchar(9) null,
+`telefone`varchar(11) null,   
+  PRIMARY KEY (`idEscola`));
+  
+  
+  create table tblProfessores (
+  `idProfessor` int primary key auto_increment,
+  `matricula` varchar(200) null,
+`txtEmail` varchar(200) null,
+fkEscola int,
+foreign key fkEscola references tblEscola(idEscola)
+  
+  
+  )
