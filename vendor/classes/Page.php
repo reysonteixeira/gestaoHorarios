3da0a34a2a7cfae0045af48b15df8b6606931c02
Reyson <reysonteixeira@gmail.com>
2025-01-15 22:54:10 -0300

    deploy

---
+++ vendor/classes/Page.php
@@ -0,0 +1,64 @@
+<?php
+
+    use Rain\Tpl;
+
+
+    class Page{
+        private $tpl;
+        private $options = [];
+        private $defaults = [
+            "data"=>[],
+            "header"=>true,
+            "footer"=>true
+        ];
+        public function __construct($opts=array(), $tpl_dir="/views/"){
+           
+            $this->options = array_merge($this->defaults, $opts);
+            $config = array(
+                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+                "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+                "debug"         => false
+               );
+
+            Tpl::configure( $config );
+            $this->tpl = new Tpl;
+            $this->setData($this->options["data"]);
+            if($this->options["header"] === true){
+                if(isset($_SESSION["acessoCovid"]["permissao"])){
+                    $this->tpl->assign("permissaoCovid", $_SESSION["acessoCovid"]["permissao"]);
+                    $this->tpl->assign("permissaoMenu", 0);
+                }else if (isset($_SESSION["admin"]["permissao"])){
+                    $this->tpl->assign("permissaoMenu",  $_SESSION["admin"]["permissao"]);
+                    $this->tpl->assign("permissaoSupervisor",  $_SESSION["admin"]["supervisor"]);
+                    $this->tpl->assign("permissaoCovid", 0);
+
+                }
+               
+                $this->tpl->draw("header");
+
+               
+                
+            }
+         
+            
+        }
+
+        public function setData($data=array()){
+            foreach($data as $key => $value){
+                $this->tpl->assign($key, $value);
+            }
+        }
+
+        public function setTpl($name, $data=array(), $returnHtlm = false){
+            $this->setData($data);
+            return ($this->tpl->draw($name, $returnHtlm));
+        }
+
+        public function __destruct(){
+            if($this->options["header"] === true){
+                $this->tpl->draw("footer");
+            }
+            
+        }
+    }
+?>
---
+++ vendor/classes/classes.php
@@ -0,0 +1,11 @@
+<?php
+
+use Slim\Http\Request;
+    require_once('Disciplinas.php');
+    require_once('Sql.php');
+    require_once('Page.php');
+    require_once('PageAdmin.php');
+    require_once('User.php');
+    require_once('Escola.php');
+    require_once('Turma.php');
+?>
