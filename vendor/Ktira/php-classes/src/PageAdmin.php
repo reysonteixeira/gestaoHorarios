0dcb707082d362d9c3ad15fb66251bd235c8237c
Reyson <reysonteixeira@gmail.com>
2025-01-15 23:27:27 -0300

    init vendor

--- vendor/Ktira/php-classes/src/PageAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace Ktira;
-    class PageAdmin extends Page{
-        public function __construct($opts=array(), $tpl_dir="/views/admin/")
-        {
-            parent::__construct($opts, $tpl_dir);
-        }
-    }
-?>
--- vendor/pagarme/pagarexport.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('PagarMe.php');
-    require_once('Anonymous.php');
-    require_once('Client.php');
-    require_once('RequestHandler.php');
-    require_once('ResponseHandler.php');
-    require_once('Routes.php');
-?>
