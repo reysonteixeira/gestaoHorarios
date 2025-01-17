75002e1850a82d056736ea87555bfd06a8595c06
Carlos Eduardo Campos Rodrigues <Carlos.admcode@gmail.com>
2025-01-17 11:45:49 -0300

    Renomea arquivos e realiza comentarios de acompanhamento

--- vendor/classes/classes.php
+++ vendor/classes/classes.php
@@ -1,10 +1,12 @@
 <?php
-    require_once('Disciplinas.php');
+    // Classe de Configuração do Frame Work:
     require_once('Sql.php');
     require_once('Page.php');
     require_once('PageAdmin.php');
+    // Classes do Projeto:
     require_once('User.php');
-    require_once('Escola.php');
     require_once('Turma.php');
-    require_once('Professores.php');
+    require_once('Escola.php');// Rescrita
+    require_once('Disciplina.php');//Rescrita
+    require_once('Professor.php');
 ?>
