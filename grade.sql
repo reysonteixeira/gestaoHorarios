2b08a0e0d3e48d164077c339b188ef5a1a9ebefb
Reyson <reysonteixeira@gmail.com>
2025-01-17 08:12:00 -0300

    inicia rotas de professores

--- grade.sql
+++ grade.sql
@@ -16,7 +16,25 @@ CREATE TABLE  tblEscola (
   create table tblProfessores (
   `idProfessor` int primary key auto_increment,
   `matricula` varchar(200) null,
+  'nomeProfessor' varchar(200) not null, 
 `txtEmail` varchar(200) null,
 fkEscola int,
 foreign key (fkEscola) references tblEscola(idEscola)
   )
+  
+  
+DELIMITER // 
+
+
+create procedure sp_SaveProfessores(pNomeProfessor varchar(200), pMatricula varchar(200), ptxtEmail varchar(200), pFkEscola int)
+begin
+INSERT INTO tblProfessores(matricula, nomeProfessor, txtEmail, fkEscola)
+VALUES (pMatricula, pNomeProfessor, ptxtEmail, pFkEscola);
+end//
+
+create procedure sp_updProfessores(pIdProfessor int, pNomeProfessor varchar(200), pMatricula varchar(200), ptxtEmail varchar(200), pFkEscola int)
+begin
+UPDATE tblProfessores
+set matricula = pMatricula, nomeProfessor = pNomeProfessor, txtEmail = ptxtEmail, fkEscola = pFkEscola
+where idProfessor = pIdProfessor;
+end//
