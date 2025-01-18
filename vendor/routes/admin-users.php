434244d771464aeedf00d1a5a4363716dd372e1b
Reyson <reysonteixeira@gmail.com>
2025-01-18 10:52:08 -0300

    ajusta nome de rotas

--- vendor/routes/admin-users.php
+++ vendor/routes/admin-users.php
@@ -1,41 +1,41 @@
 <?php
 
 
-$app->get('/admin/users', function () {
+$app->get('/admin/usuarios', function () {
     $usuario = new Usuario();
     $page = new PageAdmin();
     $usuario->listAll();
-    $page->setTpl("list-users", array("usuarios" => $usuario->listAll()));
+    $page->setTpl("list-usuarios", array("usuarios" => $usuario->listAll()));
     exit;
 });
 
-$app->get('/admin/users/:id', function ($id) {
+$app->get('/admin/usuarios/:id', function ($id) {
     $usuario = new Usuario();
     $page = new PageAdmin();
     $usuario->setIdUsuario($id);
-    $page->setTpl("edit-users", array("usuario" => $usuario->getUserById()));
+    $page->setTpl("edit-usuarios", array("usuario" => $usuario->getUserById()));
     exit;
 });
 
-$app->post('/admin/users/:id', function ($id) {
+$app->post('/admin/usuarios/:id', function ($id) {
     $usuario = new Usuario();
     $usuario->setIdUsuario($id);
     $usuario->setDados($_POST);
     $usuario->save();
-    header("location: /admin/users");
+    header("location: /admin/usuarios");
     exit;
 });
 
-$app->post('/admin/cadastrar-users', function () {
+$app->post('/admin/cadastrar-usuarios', function () {
     $usuario = new Usuario();
     $usuario->setIdUsuario(0);
     $usuario->setDados($_POST);
     $usuario->save();
-    header("location: /admin/users");
+    header("location: /admin/usuarios");
     exit;
 });
 
-$app->get('/admin/cadastrar-users', function () {
+$app->get('/admin/cadastrar-usuarios', function () {
     $page = new PageAdmin();
     $page->setTpl("cadastro-usuarios");
     exit;
